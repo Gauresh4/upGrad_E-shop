e81463f4682c995c65c60e04246a2642af413915
Gauresh <gaureshkumbhar2504@gmail.com>
2023-08-15 14:14:49 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const app = require("../E-SHOP/app");
+const serverConfigs = require("../E-SHOP/configs/server.configs");
+const dbConfigs = require("../E-SHOP/configs/db.configs");
+
+mongoose.connect(dbConfigs.DB_URL);
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("Error while connecting to DataBase");
+});
+
+db.once("open", () => {
+  console.log("Connected to DataBase");
+});
+
+app.listen(serverConfigs.PORT, () => {
+  console.log(`Server is started on port ${serverConfigs.PORT}`);
+});
