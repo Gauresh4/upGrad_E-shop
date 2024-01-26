147850977d27a8def4d7374f1aa74b7f90607055
Gauresh <gaureshkumbhar2504@gmail.com>
2024-01-26 21:47:46 +0530

    All API endpoints implemented and tested as per problem statement and found OK

--- app.js
+++ app.js
@@ -5,6 +5,5 @@ app.use(express.json());
 require("./routes/auth.route")(app);
 require("./routes/shipAddress.route")(app);
 require("./routes/product.route")(app);
-// require("./routes/order.route")(app);
 require("./routes/order.route")(app);
 module.exports = app;
--- routes/order.route.js
+++ routes/order.route.js
@@ -1,12 +1,3 @@
-// const express = require("express");
-// const orderController = require("../controllers/order.controller");
-// const authjwt = require("../middlewares/auth.middleware");
-// const { placeOrder } = require("../controllers/order.controller");
-
-// module.exports = function (app) {
-//   app.post("/orders", authjwt.verifyToken, authjwt.verifyUser, placeOrder);
-// };
-
 const { authMiddleware } = require("../middlewares");
 const orderController = require("../controllers/order.controller");
 
